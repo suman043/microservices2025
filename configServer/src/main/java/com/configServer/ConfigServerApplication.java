2d295c0c3a7ff539b21d4fb2837ca1fcb6fa9406
suman043 <suman.mohanty043@gmail.com>
2025-02-13 15:51:11 +0530

    disable config server

--- configServer/src/main/java/com/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/configServer/ConfigServerApplication.java
@@ -2,10 +2,10 @@ package com.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
